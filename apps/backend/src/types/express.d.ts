6a534905af251292f800b7392610e983a720779d
Aditi Shukla <aditishukla1676@gmail.com>
2026-01-23 12:44:56 +0530

    chore: extend express.d.ts interfaces

--- apps/backend/src/types/express.d.ts
+++ apps/backend/src/types/express.d.ts
@@ -7,3 +7,43 @@ declare global {
     }
   }
 }
+export const workflowSchema = z.object({
+    workflowId: z.string(),
+    avaialbleTriggersId: z.string(),
+    triggerMetadata:z.any().optional(),
+    triggerPositionX:z.number(),
+    triggerPositionY:z.number(),
+    userId: z.string(),
+    actions: z.array(z.object({  //this refer arrays of actions objects
+        availableActionId: z.string(),
+        actionMetadata: z.any().optional(),
+        positionX:z.number(),
+        positionY:z.number()
+    }))
+})
+
+import { Prisma, status } from "@prisma/client";
+
+export type ActionResponse = {
+  id: string;
+  workflowId: string;
+  actionId: string;
+  metadata: Prisma.JsonValue; // matches your `Json` column
+  status: status;             // enum from Prisma
+  pointer: string | null;
+}
+;
+
+
+export type IncomingEmail = {
+  fromName: string;
+  fromEmail: string;
+  to: string;
+  subject: string;
+  messageId: string;
+  date: string;
+  textBody: string;
+  htmlBody: string;
+  strippedTextReply: string;
+  attachments: Array<any>; // or a more detailed type if needed
+};
